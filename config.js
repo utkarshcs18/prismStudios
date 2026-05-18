aab8a420af4b5fb34ae988255a46cebfe508c0f9
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-05-18 21:42:03 +0530

    email service updated

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  EMAILJS_PUBLIC_KEY: "fNghUzu7BnkryEFM8",
+  EMAILJS_PUBLIC_KEY: "9l0gS6JVAApygXCin",
   EMAILJS_SERVICE_ID: "service_vuybozj",
-  EMAILJS_TEMPLATE_ID: "template_3672uhq"
+  EMAILJS_TEMPLATE_ID: "template_bfrfmjc"
 };
