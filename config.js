ae946764615e1a7fea0f96ce0eb7c073c44b10bf
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-05-18 21:37:02 +0530

    name changing operations

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  EMAILJS_PUBLIC_KEY: "fNghUzu7BnkryEFM8",
-  EMAILJS_SERVICE_ID: "service_t1b9onv",
-  EMAILJS_TEMPLATE_ID: "template_3672uhq"
+  EMAILJS_PUBLIC_KEY: "9l0gS6JVAApygXCin",
+  EMAILJS_SERVICE_ID: "service_vuybozj",
+  EMAILJS_TEMPLATE_ID: "template_bfrfmjc"
 };
